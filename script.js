7f8ffb6f5f4249f092fbb900b5a8638f21fecf66
Kyle Lin <klin12@stuy.edu>
2018-03-08 14:52:52 -0500

    Fin

--- script.js
+++ script.js
@@ -1,22 +1,77 @@
+// SVG Container
 var svgc = document.getElementById("one");
 
-var drawCircle = function(x,y){
-    var c = {circle: document.createElementNS("http://www.w3.org/2000/svg", "circle"),
-	     cx: function(){this.circle.setAttribute("cx",x)},
-	     cy: function(){this.circle.setAttribute("cy",y)},
-	     radius: function(){this.circle.setAttribute("r",10)}
-	    };
-    return c;
-}
+// Clear button
+var clearB = document.getElementById("clear");
 
-var clicky = function(e){
+// Circle Object
+var drawCircle = function (x, y) {
+    // Circle
+    var c = {
+        // Circle element
+        circle: document.createElementNS("http://www.w3.org/2000/svg", "circle"),
+        // Sets cx
+        cx: function () {
+            this.circle.setAttributeNS(null, "cx", x)
+        },
+        // Sets cy
+        cy: function () {
+            this.circle.setAttributeNS(null, "cy", y)
+        },
+        // Sets radius
+        radius: function () {
+            this.circle.setAttributeNS(null, "r", 10)
+        },
+        // Sets fill
+        black: function () {
+            this.circle.setAttributeNS(null, "fill", "black");
+        },
+        // Calls the above
+        setup: function () {
+            this.cx();
+            this.cy();
+            this.radius();
+            this.black();
+            this.circle.addEventListener('click', this.mods, true);
+        },
+        // If the color is black, change color. Else remove and add a circle to a random location
+        mods: function (e) {
+            if (this.getAttributeNS(null, "fill") == "black") {
+                this.setAttributeNS(null, "fill", genRanColor());
+            } else {
+                svgc.removeChild(this);
+                rX = Math.random() * svgc.getAttribute("width");
+                rY = Math.random() * svgc.getAttribute("height");
+                var newC = drawCircle(rX, rY);
+                svgc.appendChild(newC.circle);
+            }
+            e.stopPropagation();
+        }
+    };
+    c.setup();
+    return c;
+};
+
+// Generates a random hexcode color
+var genRanColor = function () {
+    var colorgen = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+    return colorgen;
+};
+
+// SVG Container callback function
+var clicky = function (e) {
     mouseX = e.offsetX;
     mouseY = e.offsetY;
-    var c = drawCircle(mouseX,mouseY);
-    c.cx();
-    c.cy();
-    c.radius();
+    var c = drawCircle(mouseX, mouseY);
     svgc.appendChild(c.circle);
-}
+};
 
-svgc.addEventListener("click",clicky);
+// Clear button callback function
+var clearSVG = function () {
+    while (svgc.firstChild) {
+        svgc.removeChild(svgc.firstChild);
+    }
+};
+
+svgc.addEventListener("click", clicky);
+clearB.addEventListener("click", clearSVG);
