b5f8d39a53264d9d35f6ce05f3f9f9b70640db70
Augie Murphy <augiemurphy@Augies-MacBook-Pro.local>
2018-03-08 14:50:44 -0500

    color stuff works

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 var svgc = document.getElementById("one");
 
-var drawCircle = function(x,y){
-    var c = {circle: document.createElementNS("http://www.w3.org/2000/svg", "circle"),
-	     cx: function(){this.circle.setAttribute("cx",x)},
-	     cy: function(){this.circle.setAttribute("cy",y)},
-	     radius: function(){this.circle.setAttribute("r",10)}
-	    };
+var drawCircle = function(x,y,e){
+    var n = 0;
+    var c = {
+	circle: document.createElementNS("http://www.w3.org/2000/svg", "circle"),
+	col: 0,
+	cx: function(){
+	    this.circle.setAttribute("cx",x)
+	},
+	cy: function(){
+	    this.circle.setAttribute("cy",y)
+	},
+	radius: function(){
+	    this.circle.setAttribute("r",10)
+	},
+	clear: function(e){
+	    svgc.removeChild(this.circle),
+	    drawCircle(Math.random()*490,Math.random()*490),
+	    e.stopPropagation()
+	},
+	color: function(e){
+	    this.setAttribute("fill","red"),
+	    c.circle.addEventListener("click",this.clear)
+	    e.stopPropagation()
+	},
+    };
+    c.cx();
+    c.cy();
+    c.circle.addEventListener("click", c.color);
+    c.radius();
     return c;
 }
 
 var clicky = function(e){
     mouseX = e.offsetX;
     mouseY = e.offsetY;
-    var c = drawCircle(mouseX,mouseY);
-    c.cx();
-    c.cy();
-    c.radius();
+    var c = drawCircle(mouseX,mouseY,e);
+    console.log(c.circle);
     svgc.appendChild(c.circle);
 }
 
